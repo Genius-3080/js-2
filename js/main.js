07d8f0d498ada6c4eba47aab5deb2e83cde0e6a4
Genius-3080 <abdulboriytolqinov00200@gmail.com>
2023-11-28 11:57:29 +0500

    js-2

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+var Enput = document.querySelector('.inp')
+
+function fnCheck(event){
+    event.preventDefault()
+    var exam = Enput.value
+    if(exam % 3 == 0 && exam % 7 == 0  ){
+        console.log("uchga va yettiga bolinadi");
+    }
+else if(exam % 3 == 0 ){
+    console.log("uchga bolinadi");
+}
+else if(exam % 7 == 0 ){
+    console.log("yettiga bolinadi");
+}
+
+else{
+    console.log("bo'linmaydi");
+}
+}
